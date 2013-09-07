a5854cdefee7847a508a9aa88189787289d5797f
Leikai Ma <leikai.ma@gmail.com>
2013-09-07 00:04:54 -0400

    add sign up partly

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 TestApp::Application.routes.draw do
+  get "users/new"
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new' , via: [:get, :post]
+
   match '/about', to: 'static_pages#about' , via: [:get, :post]
   match '/help', to: 'static_pages#help', via: [:get, :post]
   match '/contact', to: 'static_pages#contact', via: [:get, :post]
