4450282b9f40e5a911d631a8a6b74f39aea321f9
Leikai Ma <leikai.ma@gmail.com>
2013-09-09 01:54:06 -0400

    admin bug?

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,17 @@
 FactoryGirl.define do
 	factory :user do
-		name "Leikai Ma"
-		email "lake@example.com"
-		password "foobar"
-		password_confirmation "foobar"
-	end
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
 end
