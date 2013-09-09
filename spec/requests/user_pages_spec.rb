fe5806ec23552506ea47c444be6cc3c648c51d1e
Leikai Ma <leikai.ma@gmail.com>
2013-09-08 23:22:12 -0400

    Finish user edit, update,index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@
 #   subject {page}
 
 #   describe "signup page" do
-#   	let(:user) {FactoryGirl.create(:user)}
+#     let(:user) {FactoryGirl.create(:user)}
 #     before {visit signup_path}
 
 #     it {should have_selector('h1', text: 'Sign up')}
@@ -12,9 +12,9 @@
 #   end
 
 #   describe "profile page" do
-#   	before { visit user_path(user) }
-#   	it { should have_content(user.name)}
-#   	it {should have_title(user.name)}
+#     before { visit user_path(user) }
+#     it { should have_content(user.name)}
+#     it {should have_title(user.name)}
 #   end
 
 # end
@@ -48,7 +48,7 @@ describe "User pages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit}.not_to change(User, :count)
-      end      
+      end
     end
 
     describe "with valid information" do
@@ -62,8 +62,37 @@ describe "User pages" do
       it "should create a user" do
         expect {click_button submit}.to change(User, :count).by(1)
       end
-      
+
     end
-    
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it {should have_selector('title', text: full_title('Edit user'))}
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before  do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+      it { should  have_selector('title', text: full_title(new_name))}
+      it { should have_selector('div.alert.alert-success')}
+      it { should have_link('Sign out', href: signout_path)}
+      specify {expect(user.reload.name).to eq new_name}
+      specify {expect(user.reload.email).to eq new_email}
+
+    end
+
   end
 end
