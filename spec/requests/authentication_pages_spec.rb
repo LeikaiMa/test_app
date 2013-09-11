d17796757ee8a372f41bb2c969aa2bad71e22901
Leikai Ma <leikai.ma@gmail.com>
2013-09-11 15:40:50 -0400

    user index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -41,6 +41,7 @@ describe "Authentication" do
       before {sign_in user}
 
       it { should have_selector('title', text: user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile', href: user_path(user))}
       it {should have_link('Sign out', href: signout_path) }
       it {should_not have_link('Sign in', href: signin_path)}
@@ -94,6 +95,10 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: full_title('Sign in'))}
         end
       end
     end
