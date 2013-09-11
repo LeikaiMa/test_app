dac1395f4e6ec7a6e422b7f17c9f15ed520f1ec6
Leikai Ma <leikai.ma@gmail.com>
2013-09-11 14:26:59 -0400

    signed-in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,30 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def edit
-    @user = User.find(params[:id])
-    
+    # @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @user
@@ -32,14 +34,19 @@ class UsersController < ApplicationController
     end
   end
 
-  private 
-    def user_params
-    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    	
-    end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
 
-    before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
-    def signed_in_user
-      redirect_to signin_url, notice:"Please sign in." unless signed_in?
-    end
+  end
+
+  # before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  def signed_in_user
+    redirect_to signin_url, notice:"Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
