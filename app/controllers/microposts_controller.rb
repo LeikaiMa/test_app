10921624fb56103fd4f09f15807bd33a826ecac6
Leikai Ma <leikai.ma@gmail.com>
2013-09-12 11:16:18 -0400

    create user's post

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,12 +4,23 @@ class MicropostsController < ApplicationController
   def index
 
   end
-  
-  def create
 
+  def create
+  	@micropost = current_user.microposts.build(micropost_params)
+  	if @micropost.save
+  		flash[:success] = "Micropost created!"
+  		redirect_to root_url
+  	else
+  		render 'static_pages/home'
+  	end
   end
 
   def destroy
 
   end
+
+  private 
+  	def micropost_params
+  		params.require(:micropost).permit(:content)
+  	end
 end
