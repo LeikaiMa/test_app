335fbe401b038db4366eee25e1026b10a6a839bb
Leikai Ma <leikai.ma@gmail.com>
2013-09-12 10:42:16 -0400

    control posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ TestApp::Application.routes.draw do
   # get "users/new"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root to: 'static_pages#home'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
